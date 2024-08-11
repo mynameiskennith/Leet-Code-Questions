4e181b46c41a0a8b1c0e1f23b4f3b6bbd9a8c170
Kennith Joseph <sendmeamailatkennithjoseph@gmail.com>
2024-08-12 03:36:08 +0530

    .

--- 2418SortthePeople.py
+++ 2418SortthePeople.py
@@ -2,3 +2,17 @@ class Solution:
     def sortPeople(self, names: List[str], heights: List[int]) -> List[str]:
         return [n for _, n in sorted(zip(heights, names), reverse=True)]
 
+class Solution:
+    def sortPeople(self, names: List[str], heights: List[int]) -> List[str]:
+        dict ={}
+        n=len(heights)
+        for i in range(n):
+            dict[heights[i]]=names[i]
+        
+        heights.sort(reverse=True)
+        for i in range(n):
+            names[i] = dict[heights[i]]
+        
+        return names
+
+        print(dict)
