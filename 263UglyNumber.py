690f35f2073a282ba1cf2a44e1404d21ffbaa0ad
Kennith Joseph <sendmeamailatkennithjoseph@gmail.com>
2024-08-20 22:45:35 +0530

    .

--- 263UglyNumber.py
+++ 263UglyNumber.py
@@ -12,6 +12,17 @@ class Solution:
 
         return n==1
     
+# C++
+# class Solution {
+# public:
+#     bool isUgly(int n) {
+#         for(int i=2;i<6 && n;i++){
+#             while(n%i==0)n/=i;
+#         }
+#         return n==1;
+#     }
+# };
+
 
 # Java
 # class Solution {
